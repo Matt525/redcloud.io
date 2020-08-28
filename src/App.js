4f009cdac3da5dbb12a95a8da083f5dd5506d1a6
Matthew Statongit config --global user.name Matthew <Malanstaton@gmail.com>
2020-08-28 01:05:57 -0400

    second banner addition, shop content additions

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import Navigation from './Navigation';
 import Banner from './Banner';
 import Breakpoint from './Breakpoint';
+import Shop from './Shop';
+import SecondBanner from './SecondBanner'
+
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Banner />
       <Breakpoint />
+      <Shop />
+      <SecondBanner />
     </div>
   );
 }
